27acff87c1d671b53d211c0c6a6725d126f7ebf9
Aravindx <aravindmathi517@gmail.com>
2023-06-27 22:49:46 +0530

    HOME CHANGE

--- src/App.js
+++ src/App.js
@@ -1,30 +1,28 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import React from 'react';
-import { MovieList } from './MovieList';
-import IconButton from '@mui/material/IconButton';
-import Badge from '@mui/material/Badge';
-import ExpandLessIcon from '@mui/icons-material/ExpandLess';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import "./App.css";
+import { useEffect, useState } from "react";
+import React from "react";
+import { MovieList } from "./MovieList";
+import IconButton from "@mui/material/IconButton";
+import Badge from "@mui/material/Badge";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import { Routes, Route, useParams } from "react-router-dom";
-import { AddMovie } from './AddMovie';
-import InfoIcon from '@mui/icons-material/Info';
-import{useNavigate} from "react-router-dom";
-import Button from '@mui/material/Button';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
-import Paper from '@mui/material/Paper';
-import Brightness7Icon from '@mui/icons-material/Brightness7';
-import Brightness4Icon from '@mui/icons-material/Brightness4';
-import { BasicForm } from './BasicForm';
-import { EditMovie } from './EditMovie';
-
-
+import { AddMovie } from "./AddMovie";
+import InfoIcon from "@mui/icons-material/Info";
+import { useNavigate } from "react-router-dom";
+import Button from "@mui/material/Button";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
+import Brightness7Icon from "@mui/icons-material/Brightness7";
+import Brightness4Icon from "@mui/icons-material/Brightness4";
+import { BasicForm } from "./BasicForm";
+import { EditMovie } from "./EditMovie";
 
 // const Initial_movie_list=[
 //   {
@@ -108,141 +106,207 @@ import { EditMovie } from './EditMovie';
 // ];
 
 function App() {
-  
-  const navigate =useNavigate();
-  const[movieList,setMovieList] = useState([]);
-  const [mode,setMode] = useState('light');
+  const navigate = useNavigate();
+  const [movieList, setMovieList] = useState([]);
+  const [mode, setMode] = useState("light");
   const darkTheme = createTheme({
     palette: {
       mode: mode,
     },
   });
 
-  return(
-    
+  return (
     <ThemeProvider theme={darkTheme}>
-     <Paper style={{minHeight:"100vh",borderRadius:"0"}} elevation={0} >
-    <div className="App">
-       <AppBar position="static">
-        <Toolbar>
-            <Button color="inherit" onClick={()=>navigate("/")}>Home</Button>
-          <Button color="inherit" onClick={()=>navigate("/Movies")}>Movie</Button>
-          <Button color="inherit" onClick={()=>navigate("/AddMovie")}>AddMovie</Button>
-          <Button style={{marginLeft:"auto"}}
-          startIcon={mode==="dark"? <Brightness7Icon/>: <Brightness4Icon/>} 
-          color="inherit" onClick={()=>setMode(mode==="light"?"dark":"light")}>dark mode</Button>
-        </Toolbar>
-      </AppBar>
-         <Routes>
-         <Route path="/Movies" element={<MovieList movieList= {movieList} setMovieList={setMovieList}/>} />
-       <Route path="/" element={<Home />} /> 
-       <Route path="/Movies/:id" element={<MovieDetails movieList= {movieList} />} /> 
-       <Route path="/Movies/edit/:id" element={<EditMovie />} /> 
-      <Route path="/AddMovie" element={<AddMovie movieList= {movieList} setMovieList={setMovieList}/>} />
-      <Route path="*" element={<Error />} />
-      <Route path="/basic-form" element={<BasicForm />} />  
-      </Routes> 
-    </div>
-    </Paper >
-    </ThemeProvider> 
+      <Paper style={{ minHeight: "100vh", borderRadius: "0" }} elevation={0}>
+        <div className="App">
+          <AppBar position="static">
+            <Toolbar>
+              <Button color="inherit" onClick={() => navigate("/")}>
+                Home
+              </Button>
+              <Button color="inherit" onClick={() => navigate("/Movies")}>
+                Movie
+              </Button>
+              <Button color="inherit" onClick={() => navigate("/AddMovie")}>
+                AddMovie
+              </Button>
+              <Button
+                style={{ marginLeft: "auto" }}
+                startIcon={
+                  mode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />
+                }
+                color="inherit"
+                onClick={() => setMode(mode === "light" ? "dark" : "light")}
+              >
+                dark mode
+              </Button>
+            </Toolbar>
+          </AppBar>
+          <Routes>
+            <Route
+              path="/Movies"
+              element={
+                <MovieList movieList={movieList} setMovieList={setMovieList} />
+              }
+            />
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/Movies/:id"
+              element={<MovieDetails movieList={movieList} />}
+            />
+            <Route path="/Movies/edit/:id" element={<EditMovie />} />
+            <Route
+              path="/AddMovie"
+              element={
+                <AddMovie movieList={movieList} setMovieList={setMovieList} />
+              }
+            />
+            <Route path="*" element={<Error />} />
+            <Route path="/basic-form" element={<BasicForm />} />
+          </Routes>
+        </div>
+      </Paper>
+    </ThemeProvider>
   );
 }
-function MovieDetails(){
-  const[movie,setMovie] = useState([]);
-  useEffect(()=>{
-    fetch(`https://63899fec4eccb986e895aacd.mockapi.io/ratingapp/${id}`,{method: "GET",})
-    .then((response) => response.json())
-    .then((response) =>setMovie(response));
-  },);
-  const{id}=useParams();
-  const styles={
+function MovieDetails() {
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    fetch(`https://63899fec4eccb986e895aacd.mockapi.io/ratingapp/${id}`, {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((response) => setMovie(response));
+  });
+  const { id } = useParams();
+  const styles = {
     color: Movie.rating >= 8 ? "green" : "red",
-   }
-   const navigate =useNavigate();
-  return(
+  };
+  const navigate = useNavigate();
+  return (
     <div>
-      <iframe 
-      width="100%" 
-      height="800" 
-      src={movie.trailer}
-      title="The “FITNESS GIRLS” Exposed  - Harsh Reality of Fitness & Gym Industry 🙏🏼🤣 (TRUTH!)" 
-      frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-      allowfullscreen>
-      </iframe> 
-    <div className="movie-detaile">
-    <div className="movie-specs">
-      <h2 className="movie-name">{movie.name}</h2>
-      <p  style={styles} className="movie-rating">⭐{movie.rating}</p>
+      <iframe
+        width="100%"
+        height="800"
+        src={movie.trailer}
+        title="The “FITNESS GIRLS” Exposed  - Harsh Reality of Fitness & Gym Industry 🙏🏼🤣 (TRUTH!)"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <div className="movie-detaile">
+        <div className="movie-specs">
+          <h2 className="movie-name">{movie.name}</h2>
+          <p style={styles} className="movie-rating">
+            ⭐{movie.rating}
+          </p>
+        </div>
+        <p className="movie-summary">{movie.summary}</p>
+        <Button
+          variant="contained"
+          startIcon={<ArrowBackIcon />}
+          onClick={() => navigate(-1)}
+        >
+          back
+        </Button>
+      </div>
     </div>
-     <p className="movie-summary">{movie.summary}</p>
-     <Button variant="contained" startIcon={<ArrowBackIcon/>} onClick={()=>navigate(-1)}>back</Button>
-  </div>
-  </div> )
+  );
 }
 function Home() {
-  return (<div className='cini'>
-    <h1>WELCOME TO CINEMA📽️</h1>
-    </div>)
+  return (
+    <div className="cini">
+      <h1>WELCOME TO MOVIES APP📽️</h1>
+    </div>
+  );
 }
 function Error() {
-  return (<div >
-    <h1 className="no-404">404 ERROR:)</h1>
-    <img  className="err" src="https://www.bing.com/th/id/OGC.87bb2b84e94654e1a2efe66910814c9a?pid=1.7&rurl=https%3a%2f%2fgobarrelroll.com%2fwp-content%2fuploads%2f2018%2f09%2f404-error.gif&ehk=hEBYJM8%2fRfK17XSeZG9DgNf%2bAvyBC%2fEyf7HnHFhFtrA%3d" alt=""/>
-    </div>)
+  return (
+    <div>
+      <h1 className="no-404">404 ERROR:)</h1>
+      <img
+        className="err"
+        src="https://www.bing.com/th/id/OGC.87bb2b84e94654e1a2efe66910814c9a?pid=1.7&rurl=https%3a%2f%2fgobarrelroll.com%2fwp-content%2fuploads%2f2018%2f09%2f404-error.gif&ehk=hEBYJM8%2fRfK17XSeZG9DgNf%2bAvyBC%2fEyf7HnHFhFtrA%3d"
+        alt=""
+      />
+    </div>
+  );
 }
 
- export function Movie({Movie , id,deleteButton,editButton}) {
- const styles={
-  color: Movie.rating >= 8 ? "green" : "red",
- }
- const [show,setshow]=useState(true);
- const navigate =useNavigate();
+export function Movie({ Movie, id, deleteButton, editButton }) {
+  const styles = {
+    color: Movie.rating >= 8 ? "green" : "red",
+  };
+  const [show, setshow] = useState(true);
+  const navigate = useNavigate();
   return (
-  <Card className="movie-container">
-    <img src={Movie.poster} alt={Movie.name} className="movie-poster"/>
-    < CardContent>
-    <div className="movie-specs">
-      <h2 className="movie-name">{Movie.name} 
-      <IconButton aria-label="delete"  color="info" onClick={()=>setshow(!show)}>
-      { show ?<ExpandLessIcon />: <ExpandMoreIcon />}
-</IconButton>
-<IconButton aria-label="delete"  color="info" onClick={()=>{navigate(`/movies/${id}`)}}>
-<InfoIcon/> 
-</IconButton>
-</h2>
-      <p  style={styles}className="movie-rating">⭐{Movie.rating}</p>
-    </div>
-    {show ?  <p className="movie-summary">{Movie.summary}</p>:null}
-    </CardContent> 
-    <CardActions >
-  <Counter/>{editButton}{deleteButton}
-    </CardActions>
-  </Card>
-  )
-  }
-function Counter(){
-  let [like,setLike] = useState(0);
-  let [dislike,setDislike] = useState(0);
-  const likeStyles={
-    color: like >=10 ?"green":"black",
-  }
-  const dislikeStyles={
-    color: dislike >=10 ?"red":"black",
-  }
-  return(
+    <Card className="movie-container">
+      <img src={Movie.poster} alt={Movie.name} className="movie-poster" />
+      <CardContent>
+        <div className="movie-specs">
+          <h2 className="movie-name">
+            {Movie.name}
+            <IconButton
+              aria-label="delete"
+              color="info"
+              onClick={() => setshow(!show)}
+            >
+              {show ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+            </IconButton>
+            <IconButton
+              aria-label="delete"
+              color="info"
+              onClick={() => {
+                navigate(`/movies/${id}`);
+              }}
+            >
+              <InfoIcon />
+            </IconButton>
+          </h2>
+          <p style={styles} className="movie-rating">
+            ⭐{Movie.rating}
+          </p>
+        </div>
+        {show ? <p className="movie-summary">{Movie.summary}</p> : null}
+      </CardContent>
+      <CardActions>
+        <Counter />
+        {editButton}
+        {deleteButton}
+      </CardActions>
+    </Card>
+  );
+}
+function Counter() {
+  let [like, setLike] = useState(0);
+  let [dislike, setDislike] = useState(0);
+  const likeStyles = {
+    color: like >= 10 ? "green" : "black",
+  };
+  const dislikeStyles = {
+    color: dislike >= 10 ? "red" : "black",
+  };
+  return (
     <div className="rate">
-      <IconButton aria-label="delete" style={likeStyles} onClick={()=> setLike(like+1)}>
-      <Badge badgeContent={like} color="success">
-        👍
-    </Badge>
-     </IconButton>
-     <IconButton aria-label="delete" style={dislikeStyles} onClick={()=> setDislike(dislike+1)}>
-     <Badge badgeContent={dislike} color="warning">
-     👎
-    </Badge>
-     </IconButton>
+      <IconButton
+        aria-label="delete"
+        style={likeStyles}
+        onClick={() => setLike(like + 1)}
+      >
+        <Badge badgeContent={like} color="success">
+          👍
+        </Badge>
+      </IconButton>
+      <IconButton
+        aria-label="delete"
+        style={dislikeStyles}
+        onClick={() => setDislike(dislike + 1)}
+      >
+        <Badge badgeContent={dislike} color="warning">
+          👎
+        </Badge>
+      </IconButton>
     </div>
-  )
+  );
 }
 export default App;
-
